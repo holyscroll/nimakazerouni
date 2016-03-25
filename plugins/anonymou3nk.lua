58bd6d5877476b84b6b3f87a48dc5529e4d1f10e
nimaanonymous <senator3ite@gmail.com>
2016-03-25 06:31:31 +0430

    Create anonymou3nk.lua

---
+++ plugins/anonymou3nk.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return "با باباییم چیکار داری ؟؟ 🙂🙃"
+  end
+return {
+  description = "botfather", 
+  usage = "admin name: ",
+  patterns = {
+    "^نیما",
+  },
+  run = run
+}
+end
+— by mersad —
+— فقط برای ارسال یک یپام کاربرد دارد ! —
